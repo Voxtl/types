bac328ea885163258339ab171dd745f290086b87
VITALISED <william.castro30@gmail.com>
2021-01-27 00:18:37 +1100

    Attribution types

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,17 @@ import type { v5 as uuid } from "uuid"
 declare module "@voxtl/types" {
     type GlobalRole = null | "Moderator" | "Administrator"
 
+    type UserLinks = [
+        {
+            rel: "channel",
+            url: string
+        },
+        {
+            rel: "profile",
+            url: string
+        }
+    ]
+
     interface User {
         id: typeof uuid,
         username: string,
@@ -11,28 +22,55 @@ declare module "@voxtl/types" {
             description: string | null,
             bio: string | null
         },
-        created: Date,
         verified: boolean,
-        global_role: GlobalRole
+        global_role: GlobalRole,
+        created_at: Date,
+        links: UserLinks
+    }
+
+    interface BulkUser {
+        id: typeof uuid,
+        username: string,
+        verified: boolean,
+        global_role: GlobalRole,
+        created_at: Date,
     }
 
     interface Category {
         id: typeof uuid,
-        slug: string,
         name: string,
         visible: boolean,
-        created: Date
+        created_at: Date
     }
 
     interface Channel {
         id: typeof uuid,
         banned?: User[],
         moderators?: User[],
-        category: Category,
+        category: Category[],
         viewers: number
     }
 
     interface Self extends User {
         stream_key: string
+    }
+
+    interface _UserAttributes {
+        id: typeof uuid,
+        username: string,
+        verified: boolean,
+        global_role: GlobalRole,
+        created_at: Date,
+        updated_at: Date,
+        profile_id: typeof uuid,
+        channel_id: typeof uuid
+    }
+
+    interface _ChannelAttributes {
+        id: typeof uuid,
+        banned?: User[],
+        moderators?: User[],
+        category: Category[],
+        user_id: typeof uuid
     }
 }
